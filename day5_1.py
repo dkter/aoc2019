91e1415df230272957c01b80ff742b608d4df799
David Teresi <dkteresi+github@gmail.com>
2019-12-09 21:14:53 -0500

    day 5 part 1

---
+++ day5_1.py
@@ -0,0 +1,106 @@
+from dataclasses import dataclass
+from enum import Enum
+from itertools import zip_longest
+from typing import Callable, List, Tuple
+
+
+class ParamMode(Enum):
+    POSITION = 0
+    IMMEDIATE = 1
+
+
+def resolve(mem: List[int], val: Tuple[ParamMode, int]) -> int:
+    if val[0] == ParamMode.POSITION:
+        return mem[val[1]]
+    else:
+        return val[1]
+
+
+@dataclass
+class Operation:
+    name: str
+    opcode: int
+    nargs: int
+    do_thing: Callable
+
+
+def _op_add(self, mem, a, b, c):
+    c = c[1]
+    a, b = resolve(mem, a), resolve(mem, b)
+    mem[c] = a + b
+    return mem,
+
+def _op_mul(self, mem, a, b, c):
+    c = c[1]
+    a, b = resolve(mem, a), resolve(mem, b)
+    mem[c] = a * b
+    return mem,
+
+def _op_put(self, mem, a):
+    global stdin
+    a = a[1]
+    mem[a] = stdin.pop()
+    return mem,
+
+def _op_get(self, mem, a):
+    a = resolve(mem, a)
+    return mem, a
+
+def _op_die(self, mem):
+    return mem,
+
+
+class Instruction:
+    def __init__(self,
+                 operation: Operation,
+                 param_modes: List[ParamMode],
+                 params: List[int]):
+        self.operation = operation
+        self.params = params
+        self.param_modes = param_modes
+
+
+    @classmethod
+    def from_string(cls,
+                    string: str,
+                    params: List[int]):
+        opcode = int(string[-2:])
+        op = operations[opcode]
+        param_modes = []
+        for param_mode in string[-3::-1]:
+            if param_mode == '0':
+                param_modes.append(ParamMode.POSITION)
+            elif param_mode == '1':
+                param_modes.append(ParamMode.IMMEDIATE)
+        return cls(op, param_modes, params[:op.nargs])
+
+
+    def do_thing(self, mem):
+        params = list(zip_longest(self.param_modes, self.params, fillvalue=ParamMode.POSITION))
+        return self.operation.do_thing(self.operation, mem, *params)
+
+
+stdin = [1]
+
+operations = {
+    1: Operation("add", 1, 3, _op_add),
+    2: Operation("mul", 2, 3, _op_mul),
+    3: Operation("put", 3, 1, _op_put),
+    4: Operation("get", 4, 1, _op_get),
+    99: Operation("die", 99, 0, _op_die),
+}
+
+
+with open("day5_code.txt") as f:
+    code = f.read()
+    mem = [int(i) for i in code.split(",")]
+
+i = 0
+op = None
+while op != operations[99]:
+    instr = Instruction.from_string(str(mem[i]), params=mem[i+1:])
+    op = instr.operation
+    print(i, op.name, instr.params, instr.param_modes, sep="\t")
+    i += op.nargs + 1
+    mem, *out = instr.do_thing(mem)
+    for num in out: print(num)
